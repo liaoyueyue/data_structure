12e5476043610cca74534b310d83705b751e1ff5
liaoyueyue <973044869@qq.com>
2023-12-03 17:18:30 +0800

    study: MyLinkedList

--- MyLinkedList/src/MyLinkedList.java
+++ MyLinkedList/src/MyLinkedList.java
@@ -363,4 +363,90 @@ public class MyLinkedList {
         }
         return true;
     }
+
+    // 相交链表
+    // 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // 1. 分别求两个链表的长度
+        int lenA = 0;
+        int lenB = 0;
+        ListNode pl = headA; // p1永远指向最长的链表
+        ListNode ps = headB; // ps永远指向最短的链表
+        while(pl != null){
+            lenA++;
+            pl = pl.next;
+        }
+        while(ps != null) {
+            lenB++;
+            ps = ps.next;
+        }
+        pl = headA;
+        ps = headB;
+        int len = lenA - lenB;
+        if(len < 0){
+            pl = headB;
+            ps = headA;
+            len = lenB - lenA;
+        }
+        // 2.pl长链表 ps短链表 len为0或者正数,让长链表走差值步
+        while(len != 0) {
+            pl = pl.next;
+            len--;
+        }
+        // 3.长短链表一起走
+        // while(pl != ps && pl != null){
+        while(pl != ps){
+            pl = pl.next;
+            ps = ps.next;
+        }
+        // if(pl == null){
+        //     return null;
+        // }
+        return pl;
+    }
+
+//    环形链表
+/*    给你一个链表的头节点 head ，判断链表中是否有环。
+    如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+    如果链表中存在环 ，则返回 true 。 否则，返回 false 。*/
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return false;
+        }
+        return true;
+    }
+
+//    环形链表 II
+/*    给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+    如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+    不允许修改 链表。*/
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        slow =  head;
+        while(slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return fast;
+    }
 }
