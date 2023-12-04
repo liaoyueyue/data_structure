2ce267a15216763fb78569f865329b602af75247
liaoyueyue <973044869@qq.com>
2023-12-05 00:50:46 +0800

    sutdy: MyStack

--- MyStack/src/Test.java
+++ MyStack/src/Test.java
@@ -93,4 +93,24 @@ public class Test {
     private boolean isOperation(String op) {
         return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/");
     }
+
+    /*    栈的压入、弹出序列
+        输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+                1. 0<=pushV.length == popV.length <=1000
+                2. -1000<=pushV[i]<=1000
+                3. pushV 的所有数字均不相同*/
+    public boolean IsPopOrder(int[] pushV, int[] popV) {
+        Stack<Integer> stack = new Stack<>();
+        int j = 0;
+        for (int i = 0; i < pushV.length; i++) {
+            stack.push(pushV[i]); // 将元素压入栈中
+            while (!stack.isEmpty() && stack.peek() == popV[j]) {
+                // 如果栈顶元素和当前弹出序列元素相等，就弹出栈顶元素并移动弹出序列的指针
+                stack.pop();
+                j++;
+            }
+        }
+        // 最后判断栈是否为空，为空说明弹出序列是可能的
+        return stack.isEmpty();
+    }
 }
