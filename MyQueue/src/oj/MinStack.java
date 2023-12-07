66d4e02774a92305c7a16effb99fac990c63f029
liaoyueyue <973044869@qq.com>
2023-12-07 20:03:32 +0800

    update: MinStack

---
+++ MyQueue/src/oj/MinStack.java
@@ -0,0 +1,65 @@
+package oj;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 最小栈
+ * User: liaoyueyue
+ * Date: 2023-12-07
+ * Time: 19:43
+ */
+
+
+import java.util.Stack;
+
+/**
+ * 设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * 实现 MinStack 类:
+ * MinStack() 初始化堆栈对象。
+ * void push(int val) 将元素val推入堆栈。
+ * void pop() 删除堆栈顶部的元素。
+ * int top() 获取堆栈顶部的元素。
+ * int getMin() 获取堆栈中的最小元素。
+ */
+public class MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            if (val <= minStack.peek()) {
+                minStack.push(val);
+            }
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            int stackTopVal =  stack.pop();
+            if (stackTopVal == minStack.peek()) {
+                minStack.pop();
+            }
+        }
+    }
+
+    public int top() {
+        if (!stack.isEmpty()) {
+            return stack.peek();
+        }
+        return -1;
+    }
+
+    public int getMin() {
+        if (!minStack.isEmpty()) {
+            return minStack.peek();
+        }
+        return -1;
+    }
+}
