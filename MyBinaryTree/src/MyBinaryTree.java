81f3f98ad0055c2645ff844f789a972e1cd11b51
liaoyueyue <973044869@qq.com>
2023-12-09 00:57:40 +0800

    study: simple practice

--- MyBinaryTree/src/MyBinaryTree.java
+++ MyBinaryTree/src/MyBinaryTree.java
@@ -27,6 +27,10 @@ public class MyBinaryTree {
         binaryTree.inOrder(binaryTree.root);
         System.out.println("\n后续遍历");
         binaryTree.postOrder(binaryTree.root);
+        System.out.println();
+        System.out.println("binaryTree.size(binaryTree.root) = " + binaryTree.size(binaryTree.root));
+        binaryTree.size2(binaryTree.root);
+        System.out.println("binaryTree.nodeSize = " + binaryTree.nodeSize);
     }
 
     public void createTree() {
@@ -77,4 +81,77 @@ public class MyBinaryTree {
         postOrder(root.right);
         System.out.print(root.val + " ");
     }
+
+    // 获取树中节点的个数-子问题思路
+    int size(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return size(root.left) + size(root.right) + 1;
+    }
+
+    // 获取树中节点的个数-遍历思路
+    public static int nodeSize;
+
+    void size2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        nodeSize++;
+        size2(root.left);
+        size2(root.right);
+    }
+
+
+    // 求叶子结点个数-子问题思路
+    int getLeafNodeCount(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return root.left == null && root.right == null ? 1 : getLeafNodeCount(root.left) + getLeafNodeCount(root.right);
+    }
+
+    // 求叶子结点个数-遍历思路
+    public static int leafNodeSize;
+
+    void getLeafNodeCount2(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            leafNodeSize++;
+        }
+        getLeafNodeCount2(root.left);
+        getLeafNodeCount2(root.right);
+    }
+
+    // 获取第K层节点的个数
+    int getKLevelNodeCount(TreeNode root, int k) {
+        if (root == null) {
+            return 0;
+        }
+        if (k == 1) {
+            return 1;
+        }
+        return getKLevelNodeCount(root.left, k - 1) + getKLevelNodeCount(root.right, k - 1);
+    }
+
+    // 获取二叉树的高度
+    int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        int rightHeight = getHeight(root.right);
+        return leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+    }
+//
+//    // 检测值为value的元素是否存在
+//    TreeNode find(TreeNode root, int val);
+//
+//    //层序遍历
+//    void levelOrder(TreeNode root);
+//
+//    // 判断一棵树是不是完全二叉树
+//    boolean isCompleteTree(TreeNode root);
 }
