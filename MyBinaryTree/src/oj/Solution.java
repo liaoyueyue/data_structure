6c5a41abe81fa5b9155a9afb7de75d00f21d63d8
liaoyueyue <973044869@qq.com>
2023-12-11 23:22:58 +0800

    study: oj

--- MyBinaryTree/src/oj/Solution.java
+++ MyBinaryTree/src/oj/Solution.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 
 
-public class PreorderTraversal {
+public class Solution {
     public class TreeNode {
         int val;
         TreeNode left;
@@ -33,6 +33,36 @@ public class PreorderTraversal {
 
     }
 
+    //    另一棵树的子树
+/*
+    给你两棵二叉树 root 和 subRoot 。检验 root 中是否包含和 subRoot 具有相同结构和节点值的子树。如果存在，返回 true ；否则，返回 false 。
+    二叉树 tree 的一棵子树包括 tree 的某个节点和这个节点的所有后代节点。tree 也可以看做它自身的一棵子树。
+*/
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    // 相同的树
+/*
+    给你两棵二叉树的根节点 p 和 q ，编写一个函数来检验这两棵树是否相同。
+    如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+*/
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q != null || p != null && q == null) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+
     // 前序遍历
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> ret = new ArrayList<>();
@@ -48,7 +78,7 @@ public class PreorderTraversal {
     // 中序遍历
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> ret = new ArrayList<>();
-        if(root == null){
+        if (root == null) {
             return ret;
         }
         ret.addAll(inorderTraversal(root.left));
@@ -60,7 +90,7 @@ public class PreorderTraversal {
     // 后续遍历
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> ret = new ArrayList<>();
-        if(root == null){
+        if (root == null) {
             return ret;
         }
         ret.addAll(postorderTraversal(root.left));
