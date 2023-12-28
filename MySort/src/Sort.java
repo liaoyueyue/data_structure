7c58db2b8f776d79b58d105a967ab6635e7f124f
liaoyueyue <973044869@qq.com>
2023-12-29 00:47:17 +0800

    study: shellSort

--- MySort/src/Sort.java
+++ MySort/src/Sort.java
@@ -17,19 +17,20 @@ public class Sort {
             arr[i] = random.nextInt(100);
         }
         System.out.println("arr = " + Arrays.toString(arr));
-        sort.insertionSort(arr);
+        sort.shellSort(arr);
         System.out.println("arr = " + Arrays.toString(arr));
     }
 
     /**
      * 插入排序
      * 时间复杂度：
-     *          最好：O(n)
-     *          最坏：O(n^2)
-     *          结论：对于直接插入排序来说，数据越有序越快
-     *          场景：当数据基本上有序的时候，使用直接插入排序
+     * 最好：O(n)
+     * 最坏：O(n^2)
+     * 结论：对于直接插入排序来说，数据越有序越快
+     * 场景：当数据基本上有序的时候，使用直接插入排序
      * 空间复杂度：O(1)
      * 稳定性：稳定
+     *
      * @param arr 需要排序的数组
      */
     public void insertionSort(int[] arr) {
@@ -38,12 +39,43 @@ public class Sort {
             int j;
             for (j = i - 1; j >= 0; j--) {
                 if (arr[j] > temp) {
-                    arr[j+1] = arr[j];
+                    arr[j + 1] = arr[j];
                 } else {
                     break;
                 }
             }
             arr[j + 1] = temp;
         }
+    }
+
+    private void shell(int[] arr, int gap) {
+        for (int i = gap; i < arr.length; i++) {
+            int temp = arr[i];
+            int j;
+            for (j = i - gap; j >= 0; j -= gap) {
+                if (arr[j] > temp) {
+                    arr[j + gap] = arr[j];
+                } else {
+                    break;
+                }
+            }
+            arr[j + gap] = temp;
+        }
+    }
+
+    /**
+     * 希尔排序
+     * 时间复杂度：O(N^1.3 - N^1.5)
+     * 空间复杂度：O(1)
+     * 稳定性：不稳定
+     * @param arr 需要排序的数组
+     */
+    public void shellSort(int[] arr) {
+        int gap = arr.length;
+        while (gap > 1) {
+            shell(arr, gap);
+            gap /= 2;
+        }
+        shell(arr, 1);
     }
 }
